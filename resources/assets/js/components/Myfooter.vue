031af0ad1a1093e2db19c6667cecc971395bcc07
angad128 <30298033+angad129@users.noreply.github.com>
2018-05-04 17:42:46 +0545

    Created componets and phonebook view with those components

---
+++ resources/assets/js/components/Myfooter.vue
@@ -0,0 +1,12 @@
+<template>
+<footer class="footer">
+  <div class="container">
+    <div class="content has-text-centered">
+      <p>
+        <strong>Vuejs Phoonebook APP</strong> by <a href="#">Angad Budhathoki</a>. Access the source code at
+        <a href="http://github.com/angad128/phonebook"> Github.com/angad128</a>.
+      </p>
+    </div>
+  </div>
+</footer>
+</template>
